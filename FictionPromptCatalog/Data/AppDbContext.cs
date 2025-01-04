2307f0397890b32ee72e306e76bfbfd600ebe063
Alejandra Torres <agtorre3@yahoo.com>
2025-01-03 22:14:22 -0700

    Connect to postgres db with db context & add character table as initial migration

---
+++ FictionPromptCatalog/Data/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace FictionPromptCatalog.Data
+{
+    public class AppDbContext : DbContext
+    {
+        protected readonly IConfiguration Configuration;
+        
+        public AppDbContext(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseNpgsql(Configuration.GetConnectionString("WebApiDatabase"));
+        }
+
+        public DbSet<Character> Characters { get; set; }
+    }
+}
