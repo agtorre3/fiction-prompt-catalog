eea6fe8d12646e879fc0922fe8b94ae91d9ac8e8
Alejandra Torres <agtorre3@yahoo.com>
2025-02-23 10:47:48 -0700

    Uninstall reactstrap, install react-bootstrap
    -use react bootstrap for navbar elements

--- FictionPromptCatalog/ClientApp/src/components/Layout.js
+++ FictionPromptCatalog/ClientApp/src/components/Layout.js
@@ -1,18 +1,15 @@
-import React, {useState} from 'react';
-import { Container } from 'reactstrap';
+import React from 'react';
 import NavMenu from './NavMenu';
 
 function Layout({ children }) {
-  const displayName = Layout.name;
-
-    return (
-      <div className="bg-indigo-950">
-        <NavMenu />
-          <div className="container mx-12 mt-12 mb-12 max-w-fit">
-            {children}
-          </div>
+  return (
+    <div className="bg-indigo-950">
+      <NavMenu />
+      <div className="container mx-12 mt-12 mb-12 max-w-fit">
+        {children}
       </div>
-    );
+    </div>
+  );
 }
 
 export default Layout;
--- FictionPromptCatalog/ClientApp/src/components/NavMenu.js
+++ FictionPromptCatalog/ClientApp/src/components/NavMenu.js
@@ -1,32 +1,43 @@
-import React, {useState} from 'react';
-import { Collapse, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import './NavMenu.css';
-import Button from 'react-bootstrap/Button';
-import Overlay from 'react-bootstrap/Overlay';
-import Popover from 'react-bootstrap/Popover';
+import { Navbar, Nav, Container, Button, OverlayTrigger, Popover } from 'react-bootstrap';
 
 function NavMenu() {
-  const displayName = NavMenu.name;
+  require('react-dom');
+  window.React2 = require('react');
+  console.log(window.React1 === window.React2);
 
-    return (
-      <header>
-        <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow bg-white" container light>
-          <NavbarBrand tag={Link}>Adopt a Plot</NavbarBrand>
-            <ul className="navbar-nav flex-grow">
-              <NavItem>
-                <NavLink tag={Link} className="text-dark" to="/">Home</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink tag={Link} className="text-dark" to="/counter">Browse</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink tag={Link} className="text-dark" to="/fetch-data">Create</NavLink>
-              </NavItem>
-            </ul>
-        </Navbar>
-      </header>
-    );
+  return (
+    <header>
+      <Navbar expand="lg" className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow bg-white">
+        <Container>
+          <Navbar.Brand as={Link} to="/">Adopt a Plot</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link as={Link} to="/">Home</Nav.Link>
+              <Nav.Link as={Link} to="/counter">Browse</Nav.Link>
+              <Nav.Link as={Link} to="/fetch-data">Create</Nav.Link>
+            </Nav>
+            <OverlayTrigger
+              trigger="click"
+              placement="bottom"
+              overlay={
+                <Popover id="popover-basic">
+                  <Popover.Header as="h3">Popover bottom</Popover.Header>
+                  <Popover.Body>
+                    <strong>Holy guacamole!</strong> Check this info.
+                  </Popover.Body>
+                </Popover>
+              }
+            >
+              <Button variant="secondary">Popover on bottom</Button>
+            </OverlayTrigger>
+            </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
 }
 
 export default NavMenu;
