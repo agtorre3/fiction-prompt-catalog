708fad514c13111edd23ce494f6568d71a26df14
Alejandra Torres <agtorre3@yahoo.com>
2025-02-23 16:31:05 -0700

    Add login button with popover, add signup page

--- FictionPromptCatalog/ClientApp/src/components/NavMenu.js
+++ FictionPromptCatalog/ClientApp/src/components/NavMenu.js
@@ -1,19 +1,15 @@
 import { Link } from 'react-router-dom';
 import './NavMenu.css';
-import { Navbar, Nav, Container, Button, OverlayTrigger, Popover } from 'react-bootstrap';
+import { Navbar, Nav, Container, Button, OverlayTrigger, Popover, Form } from 'react-bootstrap';
 
 function NavMenu() {
-  require('react-dom');
-  window.React2 = require('react');
-  console.log(window.React1 === window.React2);
-
   return (
     <header>
       <Navbar expand="lg" className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow bg-white">
         <Container>
           <Navbar.Brand as={Link} to="/">Adopt a Plot</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
+          <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
               <Nav.Link as={Link} to="/">Home</Nav.Link>
               <Nav.Link as={Link} to="/counter">Browse</Nav.Link>
@@ -24,16 +20,31 @@ function NavMenu() {
               placement="bottom"
               overlay={
                 <Popover id="popover-basic">
-                  <Popover.Header as="h3">Popover bottom</Popover.Header>
+                  <Popover.Header as="h3">
+                    Not a member? <Link to="/signup">Sign up!</Link>
+                  </Popover.Header>
                   <Popover.Body>
-                    <strong>Holy guacamole!</strong> Check this info.
+                    <Form>
+                      <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control type="email" placeholder="Enter email" />
+                      </Form.Group>
+
+                      <Form.Group className="mb-3" controlId="formBasicPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" placeholder="Password" />
+                      </Form.Group>
+                      <Button variant="dark" type="submit">
+                        Submit
+                      </Button>
+                    </Form>
                   </Popover.Body>
                 </Popover>
               }
             >
-              <Button variant="secondary">Popover on bottom</Button>
+              <Button variant="outline-secondary">Login</Button>
             </OverlayTrigger>
-            </Navbar.Collapse>
+          </Navbar.Collapse>
         </Container>
       </Navbar>
     </header>
---
+++ FictionPromptCatalog/ClientApp/src/pages/SignUp.js
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import { Navigate } from 'react-router-dom';
+import { Form, Button } from 'react-bootstrap';
+
+const SignUp = () => {
+    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [redirect, setRedirect] = useState(false);
+
+    const submit = async (e) => {
+        e.preventDefault();
+
+        await fetch('http://localhost:5199/api/register', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            credentials: 'include',
+            body: JSON.stringify({
+                username,
+                email,
+                password
+            })
+        });
+
+        setRedirect(true);
+    }
+
+    if (redirect) {
+        return <Navigate to="/" />;
+    }
+
+    return (
+        <Form onSubmit={submit}>
+            <Form.Group className="mb-3" controlId="formBasicUsername">
+                <Form.Label>Username</Form.Label>
+                <Form.Control type="username" placeholder="Enter username" onChange={e => setUsername(e.target.value)} />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type="email" placeholder="Enter email" onChange={e => setEmail(e.target.value)} />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="Password" onChange={e => setPassword(e.target.value)} />
+            </Form.Group>
+            <Button variant="dark" type="submit">
+                Submit
+            </Button>
+        </Form>
+    );
+};
+
+export default SignUp;
